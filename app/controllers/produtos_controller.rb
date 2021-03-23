93a652d0bbd00b54300f108abf95adc80efc4813
Alan da Silva Alves <linuxfama@gmail.com>
2021-03-23 15:47:02 -0300

    Rails - Melhorias com o Partilas

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,5 +1,7 @@
 class ProdutosController < ApplicationController
 
+  before_action :set_produto, only: [:edit, :update, :destroy]
+
   def index
     # variavel de instancia
     @produtos = Produto.order(nome: :asc).limit 5
@@ -12,45 +14,51 @@ class ProdutosController < ApplicationController
   end
 
   def edit
-    id = params[:id]
-    @produto = Produto.find(id)
-    @departamentos = Departamento.all
-    render :new
+    renderiza :edit
   end
 
   def update
-    id = params[:id]
-    @produto = Produto.find(id)
-    valores = params.require(:produto).permit(:nome, :descricao, :preco, :quantidade, :departamento_id)
-    if @produto.update valores
+    if @produto.update produto_params
       flash[:notice] = "Produto atualizado com sucesso"
       redirect_to root_url
     else
-      @departamentos = Departamento.all
-      render :new
+      renderiza :edit
     end
   end
 
   def create
-    valores = params.require(:produto).permit(:nome, :descricao, :preco, :quantidade, :departamento_id)
-    @produto =  Produto.new valores
+    @produto =  Produto.new produto_params
     if @produto.save
       flash[:notice] = "Produto cadastrado com sucesso"
       redirect_to root_url 
     else
-      render :new
+      renderiza :new
     end
   end
 
   def destroy
-    id = params[:id]
-    Produto.destroy id
+    @produto.destroy
     redirect_to root_url
   end
 
   def busca
     @nome = params[:nome]
     @produtos = Produto.where "nome like ?", "%#{@nome}%"
+  end
+
+  private
+
+  def produto_params
+    params.require(:produto).permit(:nome, :descricao, :preco, :quantidade, :departamento_id)
+  end
+
+  def set_produto
+    @produto = Produto.find(params[:id])
+  end
+
+  def renderiza(view)
+    @departamentos = Departamento.all
+    render view
   end
 
 end
